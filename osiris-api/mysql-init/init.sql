e3f7c458de6a596747faf2ef49344e95d0d3ca5d
SofiaBelchior <sofiasbelchior@gmail.com>
2026-09-02 15:42:00 -0300

    fix(db): correct foreign key structure and naming

--- osiris-api/mysql-init/init.sql
+++ osiris-api/mysql-init/init.sql
@@ -1,6 +1,6 @@
 CREATE DATABASE osiris;
 
-use osiris;
+USE osiris;
 
 CREATE TABLE user (
     id_user INT AUTO_INCREMENT PRIMARY KEY,
@@ -45,15 +45,15 @@ CREATE TABLE file (
 
 CREATE TABLE user_memory (
     id_memory INT AUTO_INCREMENT PRIMARY KEY,
-    id_user INT,
     content TEXT NOT NULL,
     importance INT,
     created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP NOT NULL
         DEFAULT CURRENT_TIMESTAMP
         ON UPDATE CURRENT_TIMESTAMP,
-    CONSTRAINT fk_id_user
-        FOREIGN KEY (id_user)
+    fk_id_user INT,
+
+    FOREIGN KEY (fk_id_user)
         REFERENCES user(id_user)
         ON DELETE SET NULL
         ON UPDATE CASCADE
@@ -61,14 +61,14 @@ CREATE TABLE user_memory (
 
 CREATE TABLE configuration (
     id_configuration INT AUTO_INCREMENT PRIMARY KEY,
-    id_user INT NOT NULL,
     `key` VARCHAR(255) NOT NULL,
     value LONGTEXT,
     updated_at TIMESTAMP NOT NULL
         DEFAULT CURRENT_TIMESTAMP
         ON UPDATE CURRENT_TIMESTAMP,
-    CONSTRAINT fk_id_user_configuration
-        FOREIGN KEY (id_user)
+    fk_id_user INT NOT NULL,
+
+    FOREIGN KEY (fk_id_user)
         REFERENCES user(id_user)
         ON DELETE CASCADE
         ON UPDATE CASCADE
@@ -76,15 +76,15 @@ CREATE TABLE configuration (
 
 CREATE TABLE user_permission (
     id_user_permission INT AUTO_INCREMENT PRIMARY KEY,
-    id_user INT,
-    id_permission INT,
-    CONSTRAINT fk_id_user_permission
-        FOREIGN KEY (id_user)
+    fk_id_user INT,
+    fk_id_permission INT,
+
+    FOREIGN KEY (fk_id_user)
         REFERENCES user(id_user)
         ON DELETE SET NULL
         ON UPDATE CASCADE,
-    CONSTRAINT fk_id_permission
-        FOREIGN KEY (id_permission)
+
+    FOREIGN KEY (fk_id_permission)
         REFERENCES permission(id_permission)
         ON DELETE SET NULL
         ON UPDATE CASCADE
@@ -92,18 +92,18 @@ CREATE TABLE user_permission (
 
 CREATE TABLE model_installation (
     id_installation INT AUTO_INCREMENT PRIMARY KEY,
-    id_user INT,
-    id_model INT,
     installation_status VARCHAR(50),
     local_path VARCHAR(500),
     download_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    CONSTRAINT fk_id_user_model
-        FOREIGN KEY (id_user)
+    fk_id_user INT,
+    fk_id_model INT,
+
+    FOREIGN KEY (fk_id_user)
         REFERENCES user(id_user)
         ON DELETE SET NULL
         ON UPDATE CASCADE,
-    CONSTRAINT fk_id_model
-        FOREIGN KEY (id_model)
+
+    FOREIGN KEY (fk_id_model)
         REFERENCES ai_model(id_model)
         ON DELETE SET NULL
         ON UPDATE CASCADE
@@ -111,19 +111,19 @@ CREATE TABLE model_installation (
 
 CREATE TABLE agent (
     id_agent INT AUTO_INCREMENT PRIMARY KEY,
-    id_user INT NOT NULL,
-    id_model INT,
     name VARCHAR(255) NOT NULL,
     objective TEXT NOT NULL,
     execution_log VARCHAR(50) NOT NULL,
     memory INT NOT NULL,
-    CONSTRAINT fk_id_user_agent
-        FOREIGN KEY (id_user)
+    fk_id_user INT NOT NULL,
+    fk_id_model INT,
+
+    FOREIGN KEY (fk_id_user)
         REFERENCES user(id_user)
         ON DELETE CASCADE
         ON UPDATE CASCADE,
-    CONSTRAINT fk_id_model_agent
-        FOREIGN KEY (id_model)
+
+    FOREIGN KEY (fk_id_model)
         REFERENCES ai_model(id_model)
         ON DELETE SET NULL
         ON UPDATE CASCADE
@@ -131,15 +131,15 @@ CREATE TABLE agent (
 
 CREATE TABLE tool_agent (
     id_tool_agent INT AUTO_INCREMENT PRIMARY KEY,
-    id_tool INT,
-    id_agent INT,
-    CONSTRAINT fk_id_tool
-        FOREIGN KEY (id_tool)
+    fk_id_tool INT,
+    fk_id_agent INT,
+
+    FOREIGN KEY (fk_id_tool)
         REFERENCES tool(id_tool)
         ON DELETE SET NULL
         ON UPDATE CASCADE,
-    CONSTRAINT fk_id_agent
-        FOREIGN KEY (id_agent)
+
+    FOREIGN KEY (fk_id_agent)
         REFERENCES agent(id_agent)
         ON DELETE SET NULL
         ON UPDATE CASCADE
@@ -147,15 +147,15 @@ CREATE TABLE tool_agent (
 
 CREATE TABLE agent_permission (
     id_agent_permission INT AUTO_INCREMENT PRIMARY KEY,
-    id_agent INT,
-    id_permission INT,
-    CONSTRAINT fk_id_agent_permission
-        FOREIGN KEY (id_agent)
+    fk_id_agent INT,
+    fk_id_permission INT,
+
+    FOREIGN KEY (fk_id_agent)
         REFERENCES agent(id_agent)
         ON DELETE SET NULL
         ON UPDATE CASCADE,
-    CONSTRAINT fk_id_permission_agent
-        FOREIGN KEY (id_permission)
+
+    FOREIGN KEY (fk_id_permission)
         REFERENCES permission(id_permission)
         ON DELETE SET NULL
         ON UPDATE CASCADE
@@ -163,15 +163,15 @@ CREATE TABLE agent_permission (
 
 CREATE TABLE file_agent (
     id_agent_file INT AUTO_INCREMENT PRIMARY KEY,
-    id_file INT,
-    id_agent INT,
-    CONSTRAINT fk_id_file
-        FOREIGN KEY (id_file)
+    fk_id_file INT,
+    fk_id_agent INT,
+
+    FOREIGN KEY (fk_id_file)
         REFERENCES file(id_file)
         ON DELETE SET NULL
         ON UPDATE CASCADE,
-    CONSTRAINT fk_id_agent_file
-        FOREIGN KEY (id_agent)
+
+    FOREIGN KEY (fk_id_agent)
         REFERENCES agent(id_agent)
         ON DELETE SET NULL
         ON UPDATE CASCADE
@@ -179,11 +179,11 @@ CREATE TABLE file_agent (
 
 CREATE TABLE workflows (
     id_workflow INT AUTO_INCREMENT PRIMARY KEY,
-    id_user INT,
     name VARCHAR(255) NOT NULL,
     description VARCHAR(255) NOT NULL,
-    CONSTRAINT fk_id_user_workflows
-        FOREIGN KEY (id_user)
+    fk_id_user INT,
+
+    FOREIGN KEY (fk_id_user)
         REFERENCES user(id_user)
         ON DELETE SET NULL
         ON UPDATE CASCADE
@@ -191,18 +191,18 @@ CREATE TABLE workflows (
 
 CREATE TABLE node (
     id_node INT AUTO_INCREMENT PRIMARY KEY,
-    id_workflow INT NOT NULL,
     type VARCHAR(50) NOT NULL,
     name VARCHAR(255) NOT NULL,
     priority INT NOT NULL,
-    id_agent INT,
-    CONSTRAINT fk_id_workflow
-        FOREIGN KEY (id_workflow)
+    fk_id_workflow INT NOT NULL,
+    fk_id_agent INT,
+
+    FOREIGN KEY (fk_id_workflow)
         REFERENCES workflows(id_workflow)
         ON DELETE CASCADE
         ON UPDATE CASCADE,
-    CONSTRAINT fk_id_agent_node
-        FOREIGN KEY (id_agent)
+
+    FOREIGN KEY (fk_id_agent)
         REFERENCES agent(id_agent)
         ON DELETE SET NULL
         ON UPDATE CASCADE
@@ -210,15 +210,15 @@ CREATE TABLE node (
 
 CREATE TABLE file_workflow (
     id_file_workflow INT AUTO_INCREMENT PRIMARY KEY,
-    id_file INT,
-    id_workflow INT,
-    CONSTRAINT fk_id_file_workflow
-        FOREIGN KEY (id_file)
+    fk_id_file INT,
+    fk_id_workflow INT,
+
+    FOREIGN KEY (fk_id_file)
         REFERENCES file(id_file)
         ON DELETE SET NULL
         ON UPDATE CASCADE,
-    CONSTRAINT fk_id_workflow_file
-        FOREIGN KEY (id_workflow)
+
+    FOREIGN KEY (fk_id_workflow)
         REFERENCES workflows(id_workflow)
         ON DELETE SET NULL
         ON UPDATE CASCADE
@@ -226,10 +226,10 @@ CREATE TABLE file_workflow (
 
 CREATE TABLE chat (
     id_chat INT AUTO_INCREMENT PRIMARY KEY,
-    id_user INT,
     title VARCHAR(255) NOT NULL,
-    CONSTRAINT fk_id_user_chat
-        FOREIGN KEY (id_user)
+    fk_id_user INT,
+
+    FOREIGN KEY (fk_id_user)
         REFERENCES user(id_user)
         ON DELETE SET NULL
         ON UPDATE CASCADE
@@ -237,17 +237,17 @@ CREATE TABLE chat (
 
 CREATE TABLE messages (
     id_message INT AUTO_INCREMENT PRIMARY KEY,
-    id_chat INT,
-    id_model INT,
     type ENUM('user', 'assistant', 'system', 'tool') NOT NULL,
     content LONGTEXT NOT NULL,
-    CONSTRAINT fk_id_chat
-        FOREIGN KEY (id_chat)
+    fk_id_chat INT,
+    fk_id_model INT,
+
+    FOREIGN KEY (fk_id_chat)
         REFERENCES chat(id_chat)
         ON DELETE SET NULL
         ON UPDATE CASCADE,
-    CONSTRAINT fk_id_model_messages
-        FOREIGN KEY (id_model)
+
+    FOREIGN KEY (fk_id_model)
         REFERENCES ai_model(id_model)
         ON DELETE SET NULL
         ON UPDATE CASCADE
@@ -255,15 +255,15 @@ CREATE TABLE messages (
 
 CREATE TABLE file_chat (
     id_chat_file INT AUTO_INCREMENT PRIMARY KEY,
-    id_chat INT,
-    id_file INT,
-    CONSTRAINT fk_id_chat_file
-        FOREIGN KEY (id_chat)
+    fk_id_chat INT,
+    fk_id_file INT,
+
+    FOREIGN KEY (fk_id_chat)
         REFERENCES chat(id_chat)
         ON DELETE SET NULL
         ON UPDATE CASCADE,
-    CONSTRAINT fk_id_file_chat
-        FOREIGN KEY (id_file)
+
+    FOREIGN KEY (fk_id_file)
         REFERENCES file(id_file)
         ON DELETE SET NULL
         ON UPDATE CASCADE
