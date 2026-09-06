ea647fb328e5ac0cdb270587b37a74e8f6090dec
SofiaBelchior <sofiasbelchior@gmail.com>
2026-09-06 14:09:07 -0300

    fix(db): correct database script

--- osiris-api/mysql-init/init.sql
+++ osiris-api/mysql-init/init.sql
@@ -2,7 +2,7 @@ CREATE DATABASE osiris;
 
 USE osiris;
 
-CREATE TABLE user (
+CREATE TABLE `user` (
     id_user INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
     email VARCHAR(255) NOT NULL UNIQUE,
@@ -43,6 +43,91 @@ CREATE TABLE file (
     type VARCHAR(100) NOT NULL
 );
 
+CREATE TABLE agent (
+    id_agent INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    objective TEXT NOT NULL,
+    execution_log VARCHAR(50) NOT NULL,
+    system_prompt TEXT NOT NULL,
+    fk_id_user INT,
+    fk_id_model INT,
+    FOREIGN KEY (fk_id_user)
+        REFERENCES `user`(id_user)
+        ON DELETE SET NULL
+        ON UPDATE CASCADE,
+    FOREIGN KEY (fk_id_model)
+        REFERENCES ai_model(id_model)
+        ON DELETE SET NULL
+        ON UPDATE CASCADE
+);
+
+CREATE TABLE workflows (
+    id_workflow INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    description VARCHAR(255) NOT NULL,
+    fk_id_user INT,
+    FOREIGN KEY (fk_id_user)
+        REFERENCES `user`(id_user)
+        ON DELETE SET NULL
+        ON UPDATE CASCADE
+);
+
+CREATE TABLE chat (
+    id_chat INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    fk_id_user INT,
+    FOREIGN KEY (fk_id_user)
+        REFERENCES `user`(id_user)
+        ON DELETE SET NULL
+        ON UPDATE CASCADE
+);
+
+CREATE TABLE node (
+    id_node INT AUTO_INCREMENT PRIMARY KEY,
+    type VARCHAR(50) NOT NULL,
+    name VARCHAR(255) NOT NULL,
+    priority INT NOT NULL,
+    fk_id_agent INT,
+    fk_id_workflow INT,
+    FOREIGN KEY (fk_id_agent)
+        REFERENCES agent(id_agent)
+        ON DELETE SET NULL
+        ON UPDATE CASCADE,
+    FOREIGN KEY (fk_id_workflow)
+        REFERENCES workflows(id_workflow)
+        ON DELETE SET NULL
+        ON UPDATE CASCADE
+);
+
+CREATE TABLE connection (
+    id_connection INT AUTO_INCREMENT PRIMARY KEY,
+    fk_id_node_origin INT NOT NULL,
+    fk_id_node_destination INT NOT NULL,
+    FOREIGN KEY (fk_id_node_origin)
+        REFERENCES node(id_node)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE,
+    FOREIGN KEY (fk_id_node_destination)
+        REFERENCES node(id_node)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE
+);
+
+CREATE TABLE agent_memory (
+    id_memory INT AUTO_INCREMENT PRIMARY KEY,
+    content TEXT NOT NULL,
+    importance INT,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP NOT NULL
+        DEFAULT CURRENT_TIMESTAMP
+        ON UPDATE CURRENT_TIMESTAMP,
+    fk_id_agent INT,
+    FOREIGN KEY (fk_id_agent)
+        REFERENCES agent(id_agent)
+        ON DELETE SET NULL
+        ON UPDATE CASCADE
+);
+
 CREATE TABLE user_memory (
     id_memory INT AUTO_INCREMENT PRIMARY KEY,
     content TEXT NOT NULL,
@@ -52,24 +137,22 @@ CREATE TABLE user_memory (
         DEFAULT CURRENT_TIMESTAMP
         ON UPDATE CURRENT_TIMESTAMP,
     fk_id_user INT,
-
     FOREIGN KEY (fk_id_user)
-        REFERENCES user(id_user)
+        REFERENCES `user`(id_user)
         ON DELETE SET NULL
         ON UPDATE CASCADE
 );
 
 CREATE TABLE configuration (
     id_configuration INT AUTO_INCREMENT PRIMARY KEY,
+    fk_id_user INT NOT NULL,
     `key` VARCHAR(255) NOT NULL,
-    value LONGTEXT,
+    value TEXT,
     updated_at TIMESTAMP NOT NULL
         DEFAULT CURRENT_TIMESTAMP
         ON UPDATE CURRENT_TIMESTAMP,
-    fk_id_user INT NOT NULL,
-
     FOREIGN KEY (fk_id_user)
-        REFERENCES user(id_user)
+        REFERENCES `user`(id_user)
         ON DELETE CASCADE
         ON UPDATE CASCADE
 );
@@ -78,12 +161,10 @@ CREATE TABLE user_permission (
     id_user_permission INT AUTO_INCREMENT PRIMARY KEY,
     fk_id_user INT,
     fk_id_permission INT,
-
     FOREIGN KEY (fk_id_user)
-        REFERENCES user(id_user)
+        REFERENCES `user`(id_user)
         ON DELETE SET NULL
         ON UPDATE CASCADE,
-
     FOREIGN KEY (fk_id_permission)
         REFERENCES permission(id_permission)
         ON DELETE SET NULL
@@ -95,37 +176,31 @@ CREATE TABLE model_installation (
     installation_status VARCHAR(50),
     local_path VARCHAR(500),
     download_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    fk_id_user INT,
     fk_id_model INT,
-
-    FOREIGN KEY (fk_id_user)
-        REFERENCES user(id_user)
-        ON DELETE SET NULL
-        ON UPDATE CASCADE,
-
+    fk_id_user INT,
     FOREIGN KEY (fk_id_model)
         REFERENCES ai_model(id_model)
+        ON DELETE SET NULL
+        ON UPDATE CASCADE,
+    FOREIGN KEY (fk_id_user)
+        REFERENCES `user`(id_user)
         ON DELETE SET NULL
         ON UPDATE CASCADE
 );
 
-CREATE TABLE agent (
-    id_agent INT AUTO_INCREMENT PRIMARY KEY,
+CREATE TABLE api_key (
+    id_api_key INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
-    objective TEXT NOT NULL,
-    execution_log VARCHAR(50) NOT NULL,
-    memory INT NOT NULL,
+    provider VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    update_at TIMESTAMP NOT NULL
+        DEFAULT CURRENT_TIMESTAMP
+        ON UPDATE CURRENT_TIMESTAMP,
+    api_key TEXT NOT NULL,
     fk_id_user INT NOT NULL,
-    fk_id_model INT,
-
     FOREIGN KEY (fk_id_user)
-        REFERENCES user(id_user)
+        REFERENCES `user`(id_user)
         ON DELETE CASCADE
-        ON UPDATE CASCADE,
-
-    FOREIGN KEY (fk_id_model)
-        REFERENCES ai_model(id_model)
-        ON DELETE SET NULL
         ON UPDATE CASCADE
 );
 
@@ -133,12 +208,10 @@ CREATE TABLE tool_agent (
     id_tool_agent INT AUTO_INCREMENT PRIMARY KEY,
     fk_id_tool INT,
     fk_id_agent INT,
-
     FOREIGN KEY (fk_id_tool)
         REFERENCES tool(id_tool)
         ON DELETE SET NULL
         ON UPDATE CASCADE,
-
     FOREIGN KEY (fk_id_agent)
         REFERENCES agent(id_agent)
         ON DELETE SET NULL
@@ -149,12 +222,10 @@ CREATE TABLE agent_permission (
     id_agent_permission INT AUTO_INCREMENT PRIMARY KEY,
     fk_id_agent INT,
     fk_id_permission INT,
-
     FOREIGN KEY (fk_id_agent)
         REFERENCES agent(id_agent)
         ON DELETE SET NULL
         ON UPDATE CASCADE,
-
     FOREIGN KEY (fk_id_permission)
         REFERENCES permission(id_permission)
         ON DELETE SET NULL
@@ -165,43 +236,10 @@ CREATE TABLE file_agent (
     id_agent_file INT AUTO_INCREMENT PRIMARY KEY,
     fk_id_file INT,
     fk_id_agent INT,
-
     FOREIGN KEY (fk_id_file)
         REFERENCES file(id_file)
         ON DELETE SET NULL
         ON UPDATE CASCADE,
-
-    FOREIGN KEY (fk_id_agent)
-        REFERENCES agent(id_agent)
-        ON DELETE SET NULL
-        ON UPDATE CASCADE
-);
-
-CREATE TABLE workflows (
-    id_workflow INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(255) NOT NULL,
-    description VARCHAR(255) NOT NULL,
-    fk_id_user INT,
-
-    FOREIGN KEY (fk_id_user)
-        REFERENCES user(id_user)
-        ON DELETE SET NULL
-        ON UPDATE CASCADE
-);
-
-CREATE TABLE node (
-    id_node INT AUTO_INCREMENT PRIMARY KEY,
-    type VARCHAR(50) NOT NULL,
-    name VARCHAR(255) NOT NULL,
-    priority INT NOT NULL,
-    fk_id_workflow INT NOT NULL,
-    fk_id_agent INT,
-
-    FOREIGN KEY (fk_id_workflow)
-        REFERENCES workflows(id_workflow)
-        ON DELETE CASCADE
-        ON UPDATE CASCADE,
-
     FOREIGN KEY (fk_id_agent)
         REFERENCES agent(id_agent)
         ON DELETE SET NULL
@@ -212,25 +250,12 @@ CREATE TABLE file_workflow (
     id_file_workflow INT AUTO_INCREMENT PRIMARY KEY,
     fk_id_file INT,
     fk_id_workflow INT,
-
     FOREIGN KEY (fk_id_file)
         REFERENCES file(id_file)
         ON DELETE SET NULL
         ON UPDATE CASCADE,
-
     FOREIGN KEY (fk_id_workflow)
         REFERENCES workflows(id_workflow)
-        ON DELETE SET NULL
-        ON UPDATE CASCADE
-);
-
-CREATE TABLE chat (
-    id_chat INT AUTO_INCREMENT PRIMARY KEY,
-    title VARCHAR(255) NOT NULL,
-    fk_id_user INT,
-
-    FOREIGN KEY (fk_id_user)
-        REFERENCES user(id_user)
         ON DELETE SET NULL
         ON UPDATE CASCADE
 );
@@ -238,15 +263,13 @@ CREATE TABLE chat (
 CREATE TABLE messages (
     id_message INT AUTO_INCREMENT PRIMARY KEY,
     type ENUM('user', 'assistant', 'system', 'tool') NOT NULL,
-    content LONGTEXT NOT NULL,
+    content TEXT NOT NULL,
     fk_id_chat INT,
     fk_id_model INT,
-
     FOREIGN KEY (fk_id_chat)
         REFERENCES chat(id_chat)
         ON DELETE SET NULL
         ON UPDATE CASCADE,
-
     FOREIGN KEY (fk_id_model)
         REFERENCES ai_model(id_model)
         ON DELETE SET NULL
@@ -257,14 +280,42 @@ CREATE TABLE file_chat (
     id_chat_file INT AUTO_INCREMENT PRIMARY KEY,
     fk_id_chat INT,
     fk_id_file INT,
-
     FOREIGN KEY (fk_id_chat)
         REFERENCES chat(id_chat)
         ON DELETE SET NULL
         ON UPDATE CASCADE,
-
     FOREIGN KEY (fk_id_file)
         REFERENCES file(id_file)
         ON DELETE SET NULL
+        ON UPDATE CASCADE
+);
+
+CREATE TABLE usage_metrics (
+    id_metric INT AUTO_INCREMENT PRIMARY KEY,
+    input_tokens INT,
+    output_tokens INT,
+    response_time INT,
+    cpu_usage DECIMAL(10,2),
+    ram_usage DECIMAL(10,2),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    fk_id_chat INT,
+    fk_id_model INT NOT NULL,
+    fk_id_agent INT,
+    fk_id_user INT NOT NULL,
+    FOREIGN KEY (fk_id_chat)
+        REFERENCES chat(id_chat)
+        ON DELETE SET NULL
+        ON UPDATE CASCADE,
+    FOREIGN KEY (fk_id_model)
+        REFERENCES ai_model(id_model)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE,
+    FOREIGN KEY (fk_id_agent)
+        REFERENCES agent(id_agent)
+        ON DELETE SET NULL
+        ON UPDATE CASCADE,
+    FOREIGN KEY (fk_id_user)
+        REFERENCES `user`(id_user)
+        ON DELETE CASCADE
         ON UPDATE CASCADE
 );
